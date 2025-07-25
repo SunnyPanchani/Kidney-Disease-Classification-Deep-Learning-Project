73b986b59640ef785264d5dad5dbed14af9b1d3d
SunnyPanchani <sunnypanchani9007@gmail.com>
2025-07-25 11:58:13 +0530

    update pipline

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
-import getpass
-print(getpass.getuser())
+# import getpass
+# print(getpass.getuser())
+# import mlflow
+# print(mlflow.get_tracking_uri())
+
+import os
+import mlflow
+
+# Set tracking URI and credentials
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/SunnyPanchani/Kidney-Disease-Classification-Deep-Learning-Project.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "SunnyPanchani"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "51e674af3ed024048b584380ab52974ad8982d12"
+
+print("✅ Tracking URI set to:", mlflow.get_tracking_uri())
+
+
+# import os
+
+# for root, dirs, files in os.walk("mlruns"):
+#     for file in files:
+#         if file.endswith(".yaml") or file.endswith(".json"):
+#             path = os.path.join(root, file)
+#             with open(path, "r") as f:
+#                 if "jay" in f.read():
+#                     print("Found in:", path)
